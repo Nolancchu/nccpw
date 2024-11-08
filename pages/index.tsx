23651fcdb8e21cd6521f8744cdcb4d3622be0bb5
nolancchu <nolancchu@gmail.com>
2024-11-08 12:42:56 -0800

    added analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from "@/components/HomePage.jsx"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <div className="min-h-screen relative">
       <HomePage/>
+      <Analytics/>
     </div>
   );
 }
